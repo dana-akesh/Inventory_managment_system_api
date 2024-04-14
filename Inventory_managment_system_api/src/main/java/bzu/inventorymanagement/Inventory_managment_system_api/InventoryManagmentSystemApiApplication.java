138b8d2d268e2ce4853afc2e2c99c13522548946
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2024-04-14 13:14:14 +0200

    intial commit

---
+++ Inventory_managment_system_api/src/main/java/bzu/inventorymanagement/Inventory_managment_system_api/InventoryManagmentSystemApiApplication.java
@@ -0,0 +1,13 @@
+package bzu.inventorymanagement.Inventory_managment_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagmentSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagmentSystemApiApplication.class, args);
+	}
+
+}
---
+++ Inventory_managment_system_api/src/test/java/bzu/inventorymanagement/Inventory_managment_system_api/InventoryManagmentSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bzu.inventorymanagement.Inventory_managment_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagmentSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
